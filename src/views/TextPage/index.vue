026f3415ee2ed41933e2aef2a6586f55df34a2f4
AleksandrCherednichenko <acherednichenko174@gmail.com>
2023-05-10 13:13:40 +0700

    feat: -Refactoring ChatPage and TextPage;

--- src/views/TextPage/index.vue
+++ src/views/TextPage/index.vue
@@ -1,30 +1,19 @@
 <template>
-    <section class="section content-container dialogue-page">
-        <div class="preview">
-            <p>{{ $t('preview.text') }}</p>
-        </div>
+    <section class="section content-container chat__page">
+        <PreviewText text="text" />
 
-        <div class="messages">
-            <template v-for="(message, i) in chatMessages" :key="i">
-                <UIMessage
-                    :text="message.content"
-                    :side="message.role==='user' ? 'right' : 'left'"
-                />
-            </template>
+        <MessagesBlock :chat-messages="chatMessages" :loading="loading" />
 
-            <LoaderMessages v-if="loading" side="left" />
-        </div>
-
-        <div class="controls">
+        <div class="chat__page-controls">
             <UIInput
                 :value="userMessage"
-                class="controls-input"
+                class="chat__page-controls-input"
                 @input="(value)=>userMessage=value"
                 @keydown.enter="sendMessage(userMessage)"
                 @clear-input="userMessage=''"
             />
             <SubmitButton
-                class="controls-button"
+                class="chat__page-controls-button"
                 @click="sendMessage(userMessage)"
             />
         </div>
@@ -42,8 +31,8 @@ import { ref } from 'vue';
 import { TextService } from '@/services/text-service';
 import SubmitButton from '@/components/ui/buttons/SubmitButton/index.vue';
 import UIInput from '@/components/ui/UIInput/index.vue';
-import LoaderMessages from '@/components/ui/LoaderMessages/index.vue';
-import UIMessage from '@/components/ui/UIMessage/index.vue';
+import MessagesBlock from '@/components/ui/MessagesBlock/index.vue';
+import PreviewText from '@/components/ui/PreviewText/index.vue';
 
 const userMessage = ref('');
 const chatMessages = ref([]);
